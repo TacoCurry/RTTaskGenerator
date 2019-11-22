cb2104d457f1fd482a3080028731fefefcfbba5c
Chatherina423 <ysh9473@naver.com>
2019-11-22 10:22:07 +0900

    Changed fuction of MEM Uitlization

--- Memory.py
+++ Memory.py
@@ -1,11 +1,12 @@
 class Memories:
     mem_list = []
+
     mem_type = ''
     capacity= ''
     wcet_scale= ''
     power_active= ''
     power_idle= ''
-    def insert_memory(self,mem_type, capacity, wcet_scale, power_active, power_idle) -> bool:
+    def insert_memory(self, mem_type, capacity, wcet_scale, power_active, power_idle) -> bool:
         self.mem_type = mem_type
         self.capacity = capacity
         self.power_active = power_active
