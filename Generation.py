86c7d62ab71dffbf72dce4b6423c777071efd1d3
QueenCurry <yeawonjo@gmail.com>
2019-11-27 13:16:27 +0900

    Change task making methods using target total memory usage (#5)
    * Added total mem usage
    
    * Change input_task.txt
    
    Change util_target to total_memory_usage
    
    * Change input util_mem to total_mem_usage (#3)
    
    * Added total mem usage
    * Change input_task.txt
    
    Change util_target to total_memory_usage
    util_target 대신에 태스크셋의 총 메모리 요구량을 입력으로 받아서 생성하도록 변경.
    
    * Changed fuction of MEM Uitlization

--- Generation.py
+++ Generation.py
@@ -1,33 +1,26 @@
-import sys
 from Utility import Variables
 from Utility import Nansu
 from Utility import ErrorMsg
 from Memory import Memories
 
 util_cpu_1task = 0
-util_mem_1task = 0
 memreq_1task = 0
 n_mems = 2
-
 memreq_total = 0
 util_sum_cpu = 0
-memreq_total = 0
+
 
 class GenTask:
 
     def gen_task(a):
         global util_cpu_1task
         util_cpu_1task = Variables.util_cpu / Variables.n_tasks
-        global util_mem_1task
-        util_mem_1task = GenTask.get_mem_util() / Variables.n_tasks;
         global memreq_1task
-        memreq_1task = Variables.mem_total * util_mem_1task
+        memreq_1task = Variables.total_mem_usage / Variables.n_tasks
 
         print("=======================================================")
 
         print(f'util_cpu_1task:{format(util_cpu_1task, ".6f")}')
-        print(f'util_mem_1task: {format(util_mem_1task, ".6f")}')
-        print(f'get_mem_util(): {format(GenTask.get_mem_util(),".6f")}')
         print(f'memreq_1task: {format(memreq_1task, ".0f")}')
 
         print("=======================================================")
@@ -46,9 +39,9 @@ class GenTask:
         except FileNotFoundError:
             ErrorMsg.error("cannot open task_generated.txt")
 
-        print(f'full power utilization: {format(util_sum_cpu + GenTask.get_util_overhead_bymem(memreq_total),".6f")}')
-        print(f'util_sum_mem: {format(GenTask.get_util_overhead_bymem(memreq_total), ".6f")}')
-        print(f'util_sum_cpu: {format(util_sum_cpu, ".6f")}')
+
+        print(f'util_total_mem: {format(GenTask.get_util_overhead_bymem(memreq_total), ".6f")}')
+        print(f'util_total_cpu: {format(util_sum_cpu, ".6f")}')
         print(f'memreq_total: {format(memreq_total, ".0f")}')
 
 
@@ -69,47 +62,12 @@ class GenTask:
         memreq_total += memreq
 
         line = f'{wcet} {format(duration,".0f")} {format(memreq,".0f")} {format(mem_active_ratio,".6f")}\n'
-        print(f'util_sum_mem: {format(GenTask.get_util_overhead_bymem(memreq_total), ".6f")}')
-        print(f'util_sum_cpu: {format(util_sum_cpu, ".6f")}')
-
-
+        print(f'util_total_mem: {format(GenTask.get_util_overhead_bymem(memreq_total), ".6f")}')
+        print(f'util_total_cpu: {format(util_sum_cpu, ".6f")}')
         print(f'memreq_total: {format(memreq_total, ".0f")}')
 
         input_file.write(line)
 
-
-
-    @staticmethod
-    def get_mem_util() -> float:
-        global n_mems
-        util_bymem = Variables.util_target - Variables.util_cpu
-        util_mem = 1.0 / n_mems
-
-        for i in range(1,int(n_mems)):
-            util_overhead = 1 / Memories.mem_list[i].wcet_scale - 1
-            if util_bymem <= 0:
-                break
-            if util_overhead < util_bymem:
-                util_bymem -= util_overhead
-                util_mem += 1.0 / n_mems
-            else:
-                util_mem += 1.0 / n_mems * util_bymem / util_overhead
-                break
-
-        return util_mem
-
-
     def get_util_overhead_bymem(mem_used):
-            util_overhead = 0
-            global n_mems
-            mem_total_per_type = Variables.mem_total / n_mems
-            for i in range(n_mems):
-                overhead = 1 / Memories.mem_list[i].wcet_scale - 1
-                if(mem_used < mem_total_per_type):
-                    util_overhead += overhead * mem_used / mem_total_per_type
-                    break
-                else:
-                    util_overhead += overhead
-                    mem_used -= mem_total_per_type
 
-            return util_overhead
+            return mem_used/Variables.mem_total
